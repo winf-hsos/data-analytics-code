06f7d0d992ff3a8565c2e89d8e58bfa5aa22cf51
Meseth <n.meseth@hs-osnabrueck.de>
2022-05-04 00:33:10 +0200

    Added session 3

---
+++ session_3/1_plea_for_data_viz_part_2.R
@@ -0,0 +1,53 @@
+library(tidyverse)
+
+a1 <- read_csv("./data/anscombe1.csv")
+a2 <- read_csv("./data/anscombe2.csv")
+a3 <- read_csv("./data/anscombe3.csv")
+a4 <- read_csv("./data/anscombe4.csv")
+
+cor(a1$x, a1$y)
+cor(a2$x, a2$y)
+cor(a3$x, a3$y)
+cor(a4$x, a4$y)
+
+mean(a1$x)
+mean(a2$x)
+mean(a3$x)
+mean(a4$x)
+
+mean(a1$y)
+mean(a2$y)
+mean(a3$y)
+mean(a4$y)
+
+sd(a1$x)
+sd(a2$x)
+sd(a3$x)
+sd(a4$x)
+
+sd(a1$y)
+sd(a2$y)
+sd(a3$y)
+sd(a4$y)
+
+plot1 <- a1 %>% 
+  ggplot(aes(x, y)) +
+  geom_point(size = 1.5)
+
+plot2 <- a2 %>% 
+  ggplot(aes(x, y)) +
+  geom_point(size = 1.5)
+
+
+plot3 <- a3 %>% 
+  ggplot(aes(x, y)) +
+  geom_point(size = 1.5)
+
+plot4 <- a4 %>% 
+  ggplot(aes(x, y)) +
+  geom_point(size = 1.5)
+
+#install.packages("cowplot")
+library(cowplot)
+
+plot_grid(plot1, plot2, plot3, plot4, labels=c("1", "2", "3", "4"), ncol = 2, nrow = 2)
---
+++ session_3/6_themes.R
@@ -0,0 +1,49 @@
+# This script contains examples for the theme layer in ggplot2
+
+library(tidyverse)
+library(cowplot)
+
+# Load windows fonts (for later use)
+install.packages("showtext")
+library(showtext)
+showtext_auto()
+
+# Load a font from Google Fonts
+font_add_google("Special Elite", family = "special")
+
+# Get the latest data from OWID
+covid <- read_csv("https://covid.ourworldindata.org/data/owid-covid-data.csv")
+
+# Load the tweets
+tweets <- readRDS(file = "./data/tweets_ampel.rds")
+
+# Load REWE data
+rewe <- read_csv("./data/rewe_products.csv")
+
+# apply a predefined theme
+limonade %>%
+  transmute(alter = 2021 - f39_geburtsjahr, studiengang = as.factor(f44_studiengang)) %>%
+  filter(alter < 100 & studiengang != "-999") %>%
+  
+  ggplot(aes(x = alter, y = ..density..)) +
+  geom_histogram(binwidth = 1, color="#000000", fill="#009ee3", alpha = 0.8) +
+  facet_wrap(~studiengang, ncol = 2) +
+  ggtitle("Age Distribution by Study Program") +
+  labs(y = "Proportion", x = "Age") +
+  theme_cowplot()
+
+# Adjust a theme's font
+limonade %>%
+  transmute(alter = 2021 - f39_geburtsjahr, studiengang = as.factor(f44_studiengang)) %>%
+  filter(alter < 100 & studiengang != "-999") %>%
+  
+  ggplot(aes(x = alter, y = ..density..), family = "special") +
+  geom_histogram(binwidth = 1, color="#000000", fill="#009ee3", alpha = 0.8) +
+  facet_wrap(~studiengang, ncol = 2) +
+  ggtitle("Age Distribution by Study Program") +
+  labs(y = "Proportion", x = "Age") +
+  theme_cowplot() +
+  theme(title = element_text(color = "#009ee3", family = "special", size = 28),
+        axis.title.x = element_text(color = "black", size = 18),
+        axis.title.y = element_text(color = "black", size = 18),
+        )
