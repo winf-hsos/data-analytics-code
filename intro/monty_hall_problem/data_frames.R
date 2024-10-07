bca80cb3db4c9564a40620b9fa6cbb1c664d3b79
Meseth <n.meseth@hs-osnabrueck.de>
2024-10-07 10:23:04 +0200

    Expanded script

--- intro/monty_hall_problem/data_frames.R
+++ intro/monty_hall_problem/data_frames.R
@@ -19,6 +19,10 @@ monty_hall["prize_door"]
 monty_hall["contestant_choice"]
 monty_hall["decision"]
 
+monty_hall[, 1]     # first column as a vector (!)
+monty_hall[, 1:2]   # first two columns as a data frame
+monty_hall[, ncol(monty_hall)]  # last column
+
 ## Change columns data type ####
 monty_hall$decision <- as.factor(monty_hall$decision)
 levels(monty_hall$decision)
@@ -44,6 +48,9 @@ monty_hall[,1:2]
 ## First 10 rows, first 2 columns
 monty_hall[1:10,1:2]
 
+## Last row
+monty_hall[nrow(monty_hall),]
+
 ## All rows, all columns except the last one
 monty_hall[, -ncol(monty_hall)]
 
@@ -54,13 +61,15 @@ monty_hall[nrow(monty_hall), c("prize_door", "contestant_choice")]
 
 ## Filter data frames ####
 games_won <- monty_hall[monty_hall$won == TRUE, ]
-
 nrow(games_won)
 
-## Ordering data frames (increasing order)
+switch_and_won <- monty_hall[monty_hall$decision == "switch" & monty_hall$won == TRUE,]
+nrow(switch_and_won)
+
+## Ordering data frames ####
 monty_hall_sorted <- monty_hall[order(monty_hall$prize_door),]
 
-# Decreasing order
+## Decreasing order
 monty_hall[order(monty_hall$prize_door, decreasing = TRUE),]
 
 ## Load a data frame from a CSV file ####
@@ -71,9 +80,3 @@ colnames(monty_hall)
 monty_hall$contestant
 
 summary(monty_hall)
-
-
-
-
-
-
