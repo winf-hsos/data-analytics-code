5249a47ff9f547a0ca400e09c190cf23440260b3
Meseth <n.meseth@hs-osnabrueck.de>
2024-11-22 09:28:29 +0100

    Added factor examples

--- part_0/factors.R
+++ part_0/factors.R
@@ -1,22 +1,31 @@
 # Introduction to factors ####
 
-## Create a factor ####
-weight_category <- factor(c("heavy", "medium", "light", "medium", "heavy"))
+library(tidyverse)
 
-print(weight_category)
+## Create a factor ####
+# Create a vector
+weight_vector <- c("heavy", "medium", "light", "medium", "heavy", "heavy", "medium", "heavy")
+
+# Convert to factor
+weight_category <- factor(weight_vector)
+weight_category
+
+# Using as_factor from Tidyverse to preserve from data
+weight_category <- as_factor(weight_vector)
+weight_category
 
 ## Check the levels of the factor ####
 levels(weight_category)
 
-
 ## Create a factor with specific levels ####
 weight_category_ordered <- factor(c("heavy", "medium", "light", "medium", "heavy"),
-                                  levels = c("light", "medium", "heavy"))
+                                  levels = c("light", "medium", "heavy", "super-heavy"))
 
-print(weight_category_ordered)
+levels(weight_category_ordered)
 
-## Reorder the factor levels ####
-weight_category_reordered <- factor(weight_category, levels = c("light", "medium", "heavy"))
+## Reorder the factor levels manually ####
+weight_category_reordered <- 
+  factor(weight_category, levels = c("light", "medium", "heavy"))
 
 print(weight_category_reordered)
 
@@ -29,3 +38,9 @@ print(ordered_weight_category)
 
 # Check if one factor level is greater than another
 ordered_weight_category[1] < ordered_weight_category[2]
+
+# Forcats
+fct_lump(weight_category)
+fct_infreq(weight_category)
+fct_relevel(weight_category, "light")
+fct_rev(weight_category)
--- part_1/1_3_data_visualization/1_3_1_simple_plots/1_plea_for_data_viz_part_1.R
+++ part_1/1_3_data_visualization/1_3_1_simple_plots/1_plea_for_data_viz_part_1.R
@@ -3,7 +3,15 @@ library(tidyverse)
 some_data <- read_csv("data/some_data.csv")
 
 some_data |> 
-  print(n = 142)
+  print(n = 200)
+
+mean(some_data$x)
+mean(some_data$y)
+
+sd(some_data$x)
+sd(some_data$y)
+
+cor(some_data$x, some_data$y)
 
 ggplot(some_data) +
   aes(x, y) +
